a95a7e082ba6c8447c6d5c372141e024ff3fbc6b
Brad Pasanek <bmp7e@virginia.edu>
2022-03-07 14:05:37 -0500

    resetting variables

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ import { PoemPuzzle } from './Types';
 import { idText, isTemplateExpression } from 'typescript';
 
 var PoemPuzzles = require('./mockData.JSON') // imports JSON file?
-// const poems = Object.keys(PoemPuzzles) // convert to an array? (Is already an array?)
+const poems = Object.keys(PoemPuzzles) // convert to an array? (Is already an array?)
 
 // // // testing map function -- not sure JSON coming in right
 // // But this doesn't seem to work either
-const poems = [
-  { id: 1, title: "Cobbing", text: "wan do tree fear fife seeks siphon eat neighing den elephan twirl" },
-  { id: 2, title: "Naylor", text: "Hero, hero, hero Hero, hero... run! Hero, run hero. Hero run run! Run, hero hero Run, hero, run! Run run hero Run run run!" }
-]
+// const poems = [
+//   { id: 1, title: "Cobbing", text: "wan do tree fear fife seeks siphon eat neighing den elephan twirl" },
+//   { id: 2, title: "Naylor", text: "Hero, hero, hero Hero, hero... run! Hero, run hero. Hero run run! Run, hero hero Run, hero, run! Run run hero Run run run!" }
+// ]
 
 // FOR HOMEWORK: Display mock data on the App Level 
 // Create a variable that is a PoemPuzzle array, should match the type
